158534ca7f0bcaa4e361154997646c46724c159c
Trane-N-lab <fatemenabighanbari1385@gmail.com>
2026-09-21 22:36:20 +0330

    Update Console.hpp

--- src/Console.hpp
+++ src/Console.hpp
@@ -1,13 +1,62 @@
-#pragma once
-#include "Board.hpp"
-#include <string>
+#include "Console.hpp"
 
-class Console {
-public:
-    static void setupUtf8();
-    static void clear();
-    static bool keyAvailable();
-    static int readKey();
-    static void sleepMs(unsigned milliseconds);
-    static void draw(const Board& board, int score, const std::string& message = {});
-};
+#include <conio.h>
+#include <iostream>
+#include <windows.h>
+
+void Console::setupUtf8() {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+}
+
+void Console::clear() {
+    system("cls");
+}
+
+bool Console::keyAvailable() {
+    return _kbhit() != 0;
+}
+
+int Console::readKey() {
+    return _getch();
+}
+
+void Console::sleepMs(unsigned milliseconds) {
+    Sleep(milliseconds);
+}
+
+void Console::draw(const Board& board, int score, const std::string& message) {
+    clear();
+
+    std::cout << "+";
+
+    for (int column = 0; column < board.width(); ++column) {
+        std::cout << "---+";
+    }
+
+    std::cout << '\n';
+
+    for (int row = 0; row < board.height(); ++row) {
+        std::cout << "|";
+
+        for (int column = 0; column < board.width(); ++column) {
+            std::cout << ' ' << board.at(row, column) << " |";
+        }
+
+        std::cout << '\n';
+
+        std::cout << "+";
+
+        for (int column = 0; column < board.width(); ++column) {
+            std::cout << "---+";
+        }
+
+        std::cout << '\n';
+    }
+
+    std::cout << "Score: " << score << '\n';
+
+    if (!message.empty()) {
+        std::cout << message << '\n';
+    }
+}
